f3d62335b6929dd9af7b538cf9c27a92b646c2a9
Krishna T <krishna.telgave@gmail.com>
2024-10-08 13:55:25 +0530

    refactoring

--- src/com/paytech/jakarta/filters/ResponseHeaderFilter.java
+++ src/com/paytech/jakarta/filters/ResponseHeaderFilter.java
@@ -1,4 +1,4 @@
-package com.paytech.tomcat.javaxfilters;
+package com.paytech.jakarta.filters;
 
 import java.io.IOException;
 import java.util.Enumeration;
@@ -6,15 +6,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public final class ResponseHeaderFilter implements Filter {
 
